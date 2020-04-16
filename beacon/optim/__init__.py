01298d6ec3e216d91d5faa0d1bc72a509267e50b
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-04-16 21:21:37 +0200

    Adagrad optimizer.

--- beacon/optim/__init__.py
+++ beacon/optim/__init__.py
@@ -2,3 +2,4 @@ from beacon.optim.sgd import SGD
 from beacon.optim.adam import Adam
 from beacon.optim.momentum import Momentum
 from beacon.optim.nesterov import Nesterov
+from beacon.optim.adagrad import Adagrad
---
+++ beacon/optim/adagrad.py
@@ -0,0 +1,15 @@
+from beacon.optim.optimizer import Optimizer
+from beacon.tensor import functions as fn
+
+class Adagrad(Optimizer):
+
+    def __init__(self, parameters, lr=0.01, epsilon=1e-8):
+        super().__init__(parameters, lr=lr)
+        self.epison = epsilon
+        self.G = [fn.zeros_like(p) for p in self.parameters]
+
+    def _step(self):
+        grads = [p.grad for p in self.parameters]
+        for p, g, gs in zip(self.parameters, grads, self.G):
+            gs += fn.square(g)
+            p -= self.lr * g / fn.sqrt(gs + self.epison)
