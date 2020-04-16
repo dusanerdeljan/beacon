19beec0a82b01f9b661b68dcb1d1a53377361823
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-04-16 21:35:11 +0200

    RMSProp optimizer.

--- beacon/optim/__init__.py
+++ beacon/optim/__init__.py
@@ -4,3 +4,4 @@ from beacon.optim.momentum import Momentum
 from beacon.optim.nesterov import Nesterov
 from beacon.optim.adagrad import Adagrad
 from beacon.optim.adadelta import Adadelta
+from beacon.optim.rmsprop import RMSProp
