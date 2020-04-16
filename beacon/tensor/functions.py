f1ee35b9ed860982fa0fa076f9380c8fafa2ec5b
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-04-16 13:59:57 +0200

    Implemented more differentiable operators.

--- beacon/tensor/functions.py
+++ beacon/tensor/functions.py
@@ -121,9 +121,29 @@ def maximum(t1: Tensor, t2: Tensor):
     def max_grad(x, z, y):
         return (x == z) / (1.0 + (x == y))
     if t1.requires_grad:
-        nodes.append(Tensor.ComputationalGraphNode(tensor=t1, df=lambda x: x * max_grad(t1.data, data, t2.data)))
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t1, df=lambda x: broadcast(t1.data, x * max_grad(t1.data, data, t2.data))))
     if t2.requires_grad:
-        nodes.append(Tensor.ComputationalGraphNode(tensor=t2, df=lambda x: x * max_grad(t2.data, data, t1.data)))
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t2, df=lambda x: broadcast(t2.data, x * max_grad(t2.data, data, t1.data))))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def minimum(t1: Tensor, t2: Tensor):
+    data = np.minimum(t1.data, t2.data)
+    requires_grad = t1.requires_grad or t2.requires_grad
+    nodes = []
+    def min_grad(x, z, y):
+        return (x == z) / (1.0 + (x == y))
+    if t1.requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t1, df=lambda x: broadcast(t1.data, x * min_grad(t1.data, data, t2.data))))
+    if t2.requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t2, df=lambda x: broadcast(t2.data, x * min_grad(t2.data, data, t1.data))))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def clip(t: Tensor, min_val, max_val):
+    data = np.clip(t.data, min_val, max_val)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: x * np.logical_and(data != min_val, data != max_val)))
     return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
 
 def power(t1: Tensor, t2: Tensor):
@@ -135,5 +155,69 @@ def power(t1: Tensor, t2: Tensor):
         df=lambda x: broadcast(t1.data, x*t2.data*(t1.data**np.where(t2.data, t2.data-1, 1.)))))
     if t2.requires_grad:
         nodes.append(Tensor.ComputationalGraphNode(tensor=t2, 
-        df=lambda x: broadcast(t2.data, x * np.log((np.where(t1.data, t1.data, 1.)) * t1.data**t2.data))))
+        df=lambda x: broadcast(t2.data, x * np.log((np.where(t1.data, t1.data, 1.)) * data))))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def abs(t: Tensor):
+    data = np.abs(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: np.sign(t.data)*x))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def log(t: Tensor):
+    data = np.log(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: x / t.data))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def sqrt(t: Tensor):
+    data = np.sqrt(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: 0.5*x*(t.data**-0.5)))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def square(t: Tensor):
+    data = np.square(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: 2*x*t.data))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def reshape(t: Tensor, shape):
+    data = np.reshape(t.data, newshape=shape)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: np.reshape(x, np.shape(t.data))))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def sin(t: Tensor):
+    data = np.sin(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: x * np.cos(t.data)))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def cos(t: Tensor):
+    data = np.cos(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: -x * np.sin(t.data)))
+    return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
+
+def tan(t: Tensor):
+    data = np.tan(t.data)
+    requires_grad = t.requires_grad
+    nodes = []
+    if requires_grad:
+        nodes.append(Tensor.ComputationalGraphNode(tensor=t, df=lambda x: x / np.cos(t.data)**2))
     return Tensor(data=data, requires_grad=requires_grad, nodes=nodes)
