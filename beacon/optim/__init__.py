bd3e4a66fe60a73c86a6bd1f4e36d2ff251ae46b
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-04-16 21:16:11 +0200

    Nesterov optimizer.

--- beacon/optim/__init__.py
+++ beacon/optim/__init__.py
@@ -1,3 +1,4 @@
 from beacon.optim.sgd import SGD
 from beacon.optim.adam import Adam
 from beacon.optim.momentum import Momentum
+from beacon.optim.nesterov import Nesterov
---
+++ beacon/optim/nesterov.py
@@ -0,0 +1,16 @@
+from beacon.optim.optimizer import Optimizer
+from beacon.tensor import functions as fn
+
+class Nesterov(Optimizer):
+
+    def __init__(self, parameters, lr=0.01, momentum=0.9):
+        super().__init__(parameters, lr=lr)
+        self.momentum = fn.to_tensor(momentum)
+        self.vs = [fn.zeros_like(p) for p in self.parameters]
+
+    def _step(self):
+        grads = [p.grad for p in self.parameters]
+        for p, g, v in zip(self.parameters, grads, self.vs):
+            prev = v
+            v = self.momentum * v - self.lr * g
+            p -= self.momentum * prev - (1.0 + self.momentum) * v
